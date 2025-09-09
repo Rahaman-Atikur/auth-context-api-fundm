9c6a014441f809602a9baf13fecc6b04ee7c005b
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-09 21:56:05 +0100

    Routing Setting Done

---
+++ src/Componets/Roots.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Roots = () => {
+    return (
+        <div>
+            <h2>This is Root and Finding the Concepts as well</h2>
+        </div>
+    );
+};
+
+export default Roots;
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,6 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import Roots from "../Componets/Roots";
+import Home from "../Componets/Home/Home";
+import About from "../Componets/About/About";
 
- export const router = createBrowserRouter([{
+export const router = createBrowserRouter([
+  {
     path: "/",
-    element: <div>Hello World</div>
-}])
+    Component: Roots,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      { path: "about", Component: About },
+    ],
+  },
+]);
