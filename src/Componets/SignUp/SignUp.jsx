7eb008dc9cdb6ffc4a88f9925036dc8fc72f249f
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-10 21:26:49 +0100

    Now Form is Ready

--- src/Componets/SignUp/SignUp.jsx
+++ src/Componets/SignUp/SignUp.jsx
@@ -17,16 +17,26 @@ const SignUp = () => {
             <form onSubmit={handleSubmit} noValidate="" action="" className="space-y-12">
                 <div className="space-y-4">
                     <div>
+                        <label htmlFor="email" className="block mb-2 text-sm">Name</label>
+                        <input type="text" name="name" id="email" placeholder="Joe Ward" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+                    </div>
+                    <div>
                         <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
                         <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
                     </div>
                     <div>
                         <div className="flex justify-between mb-2">
                             <label htmlFor="password" className="text-sm">Password</label>
-                            <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">Forgot password?</a>
                         </div>
                         <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
                     </div>
+                    <div>
+                        <div className="flex justify-between mb-2">
+                            <label htmlFor="password" className="text-sm">Password</label>
+                        </div>
+                        <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+                    </div>
+
                 </div>
                 <div className="space-y-2">
                     <div>
