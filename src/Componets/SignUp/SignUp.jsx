8de856422f889c03fb8c4c5acf32838d34c3129b
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-10 21:14:08 +0100

    SignUp Form ready and other work is undone

--- src/Componets/SignUp/SignUp.jsx
+++ src/Componets/SignUp/SignUp.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { NavLink } from 'react-router';
 
 const SignUp = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password);
+    }
     return (
-        <div>
-            <h2>This is SignUp</h2>
+        <div className="flex flex-col max-w-md mx-auto p-6 rounded-md bg-gray-200 mt-4">
+            <div className="mb-8 text-center">
+                <h1 className="my-3 text-4xl font-bold">Sign Up</h1>
+                <p className="text-sm dark:text-gray-600">Sign Up to access your account</p>
+            </div>
+            <form onSubmit={handleSubmit} noValidate="" action="" className="space-y-12">
+                <div className="space-y-4">
+                    <div>
+                        <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
+                        <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+                    </div>
+                    <div>
+                        <div className="flex justify-between mb-2">
+                            <label htmlFor="password" className="text-sm">Password</label>
+                            <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">Forgot password?</a>
+                        </div>
+                        <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" />
+                    </div>
+                </div>
+                <div className="space-y-2">
+                    <div>
+                        <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign Up</button>
+                    </div>
+                    <p className="px-6 text-sm text-center dark:text-gray-600 mr-5">Already Have an account ?
+                        <NavLink to="/signin" rel="noopener noreferrer" href="#" className="hover:underline dark:text-violet-600 mr-5">Sign In</NavLink>
+                    </p>
+                </div>
+            </form>
         </div>
     );
 };
-
 export default SignUp;
