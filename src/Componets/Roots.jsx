0d0551d10938aefe0a6ffa1910815f14a0257225
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-09-12 23:58:24 +0100

    SignUp Runnign

--- src/Componets/Roots.jsx
+++ src/Componets/Roots.jsx
@@ -1,18 +1,28 @@
 import React, { createContext } from 'react';
 import { Outlet } from 'react-router';
 import Navbar from './Navbar/Navbar';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../Firebase/firebase.config';
  export const valueContext = createContext();
 const Roots = () => {
-    const a = 10;
+    // const a = 10;
+    const loveFun = ()=>{
+        console.log("Love Function");
+    }
+    // For Sign Up 
+    const handleSignUp  = (email,password)=>{
+        createUserWithEmailAndPassword(auth,email,password)
+    }
+    const contextValues = {
+        handleSignUp
+    }
     return (
         <div>
             <Navbar></Navbar>
-            <valueContext.Provider value={a}>
+            <valueContext.Provider value={handleSignUp}>
                 <Outlet></Outlet>
             </valueContext.Provider>
-
         </div>
     );
 };
-
 export default Roots;
--- src/Componets/SignUp/SignUp.jsx
+++ src/Componets/SignUp/SignUp.jsx
@@ -5,17 +5,34 @@ import { auth } from '../../Firebase/firebase.config';
 const SignUp = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
+
+
         const name = e.target.name.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(name, email, password);
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((result) => {
-                console.log("User created:", result.user);
+
+        // createUserWithEmailAndPassword(auth, email, password)
+        //     .then((result) => {
+        //         console.log("User created:", result.user);
+        //     })
+        //     .catch((error) => {
+        //         console.error("Error:", error.message);
+        //     });
+
+        if (password.length < 6) {
+            alert("at least Six character");
+            return;
+        }
+
+        handleSignUp(email, password)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                alert("User Sign Up Success");
             })
             .catch((error) => {
-                console.error("Error:", error.message);
-            });
+                console.log(error)
+            })
     };
     return (
         <div className="flex flex-col max-w-md mx-auto p-6 rounded-md bg-gray-200 mt-4">
@@ -27,38 +44,38 @@ const SignUp = () => {
                 <div className="space-y-4">
                     <div>
                         <label htmlFor="name" className="block mb-2 text-sm">Name</label>
-                        <input 
-                            type="text" 
-                            name="name" 
-                            id="name" 
-                            placeholder="Joe Ward" 
-                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" 
+                        <input
+                            type="text"
+                            name="name"
+                            id="name"
+                            placeholder="Joe Ward"
+                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
                         />
                     </div>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
-                        <input 
-                            type="email" 
-                            name="email" 
-                            id="email" 
-                            placeholder="leroy@jenkins.com" 
-                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" 
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            placeholder="leroy@jenkins.com"
+                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
                         />
                     </div>
                     <div>
                         <label htmlFor="password" className="text-sm block mb-2">Password</label>
-                        <input 
-                            type="password" 
-                            name="password" 
-                            id="password" 
-                            placeholder="*****" 
-                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800" 
+                        <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="*****"
+                            className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800"
                         />
                     </div>
                 </div>
                 <div className="space-y-2">
-                    <button 
-                        type="submit" 
+                    <button
+                        type="submit"
                         className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50"
                     >
                         Sign Up
